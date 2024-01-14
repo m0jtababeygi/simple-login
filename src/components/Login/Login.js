4df806f70b5bec90317850a71b46a3b94f2462b7
m.beygi <M.Beygi@sadad.co.ir>
2024-01-14 09:25:14 +0330

    useEffect cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,19 +11,19 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log('checking for validation');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6 
-  //     );
-  //   }, 500);
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('checking for validation');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6 
+      );
+    }, 500);
 
-  //   return () => {
-  //     console.log('clean up');
-  //     clearTimeout(identifier);
-  //   }
-  // }, [enteredEmail, enteredPassword]);
+    return () => {
+      console.log('clean up');
+      clearTimeout(identifier);
+    }
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
